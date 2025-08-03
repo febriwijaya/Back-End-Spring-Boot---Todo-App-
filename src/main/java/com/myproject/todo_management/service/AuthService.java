4246f0a48f1f310b948965b6e9dc7bc5662e800e
febri wijaya <59653659+febriwijaya@users.noreply.github.com>
2025-08-03 15:04:36 +0700

    fixed role

--- src/main/java/com/myproject/todo_management/service/AuthService.java
+++ src/main/java/com/myproject/todo_management/service/AuthService.java
@@ -1,10 +1,11 @@
 package com.myproject.todo_management.service;
 
+import com.myproject.todo_management.dto.JwtAuthResponse;
 import com.myproject.todo_management.dto.LoginDto;
 import com.myproject.todo_management.dto.RegisterDto;
 
 public interface AuthService {
     String register(RegisterDto registerDto);
 
-    String login(LoginDto loginDto);
+    JwtAuthResponse login(LoginDto loginDto);
 }
--- src/main/java/com/myproject/todo_management/service/impl/AuthServiceImpl.java
+++ src/main/java/com/myproject/todo_management/service/impl/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package com.myproject.todo_management.service.impl;
 
+import com.myproject.todo_management.dto.JwtAuthResponse;
 import com.myproject.todo_management.dto.LoginDto;
 import com.myproject.todo_management.dto.RegisterDto;
 import com.myproject.todo_management.entity.Role;
@@ -19,6 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -62,7 +64,7 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
-    public String login(LoginDto loginDto) {
+    public JwtAuthResponse login(LoginDto loginDto) {
 
         Authentication authentication =  authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 loginDto.getUsernameOrEmail(),
@@ -73,6 +75,23 @@ public class AuthServiceImpl implements AuthService {
 
         String token = jwtTokenProvider.generateToken(authentication);
 
-        return token;
+        Optional<User> userOptional =  userRepository.findByUsernameOrEmail(loginDto.getUsernameOrEmail(), loginDto.getUsernameOrEmail());
+
+        String role = null;
+        if(userOptional.isPresent()) {
+            User loggedInUser = userOptional.get();
+            Optional<Role> optionalRole = loggedInUser.getRoles().stream().findFirst();
+
+            if (optionalRole.isPresent()) {
+                Role userRole = optionalRole.get();
+                role = userRole.getName();
+            }
+         }
+
+        JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
+        jwtAuthResponse.setRole(role);
+        jwtAuthResponse.setAccessToken(token);
+
+        return jwtAuthResponse;
     }
 }
